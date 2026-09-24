c6150fdb24810079d31b211950ff4914592c9426
s6912041610507-glitch <s6912041610507@email.kmutnb.ac.th>
2026-09-24 11:19:57 +0000

    Update scores

--- compro_week_10.c
+++ compro_week_10.c
@@ -9,7 +9,7 @@ int main()
         "Tankhwan"
     };
 
-    /* คะแนนชุดใหม่ */
+
     float math[3] = {85.00, 72.00, 64.00};
     float phy[3]  = {78.50, 91.00, 69.50};
     float chem[3] = {92.00, 76.50, 88.00};
@@ -17,12 +17,12 @@ int main()
     float avgMath, avgPhy, avgChem;
     int i;
 
-    /* คำนวณค่าเฉลี่ย */
+
     avgMath = (math[0] + math[1] + math[2]) / 3;
     avgPhy  = (phy[0] + phy[1] + phy[2]) / 3;
     avgChem = (chem[0] + chem[1] + chem[2]) / 3;
 
-    /* แสดงผล */
+
     printf("============================================================\n");
     printf("Student (length)                 Math       Phy       Chem\n");
     printf("------------------------------------------------------------\n");
